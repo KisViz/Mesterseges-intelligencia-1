b2f71063780c93e1994ce4703093a020d1e16617
h378775 <h378775@stud.u-szeged.hu>
2025-11-08 11:05:01 +0100

    initial commit

---
+++ Agent.java
@@ -0,0 +1,21 @@
+///Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.,h378775@stud.u-szeged.hu
+
+import java.util.Random;
+
+import game.mario.Direction;
+import game.mario.MarioGame;
+import game.mario.MarioPlayer;
+import game.mario.utils.MarioState;
+
+public class Agent extends MarioPlayer {
+    public Agent(int color, Random random, MarioState state) {
+        super(color, random, state);
+    }
+
+    @Override
+    public Direction getDirection(long remainingTime) {
+        Direction action = new Direction(MarioGame.DIRECTIONS[random.nextInt(MarioGame.DIRECTIONS.length)]);
+        state.apply(action);
+        return action;
+    }
+}
